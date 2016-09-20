0b8ed6156f85b85e530d02642a53c4afec6fc70e
Brian Doane <brianmdoane@gmail.com>
2016-09-20 13:23:54 -0500

    more index.js

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,42 @@ const bcrypt = require('bcrypt')
 const router = Router()
 
 const User = require('../models/user')
+
+router.get('/', (req, res) =>
+	res.render('index')
+)
+
+router.get('login', (req, res) =>
+	res.render('login', {page: 'Login'})
+)
+
+router.get('register', (req, res) =>
+	res.render('register', {page: 'Register'})
+)
+
+router.post('/login', ({session, body: {email, password}}, res, err) => {
+	User.findOne({ email })
+	.then(user => {
+		if (user) {
+			return new Promise((resolve, reject) => {
+				bcrypt.compare(password, user.password, (err, matches) => {
+					if (err) {
+						reject(err)
+					} else {
+						resolve(matches)
+					}
+				})
+			})
+		} else {
+			res.render('login', {msg: 'Email does not exist in our system'})
+		}
+	})
+	.then((matches) => {
+		if (matches) {
+			session.email = email
+			res.redirect('/')
+		} else {
+			res.render('login', {msg: 'Password does not match'}
+		}
+	})
+})
